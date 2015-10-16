4afebd1ce4d770c114a483b473bca2a5ecb299d1
Peter Hurford <peter@peterhurford.com>
2015-10-16 16:46:39 -0500

    Restyle

--- tests/testthat/test-dbtest.R
+++ tests/testthat/test-dbtest.R
@@ -8,9 +8,9 @@ test_that("our actual call will error", {
   expect_error(call_to_actual_database_oh_no())
 })
 
-test_that("we can stub our actual db call with the test connection", {
-  expect_true(with_test_db({ call_to_actual_database_oh_no(); TRUE }))
-})
+with_test_db({
+  test_that("we can stub our actual db call with the test connection", {
+  expect_true(call_to_actual_database_oh_no(); TRUE) }) })
 
 test_that("with_test_db produces a test_con object", {
   expect_equal(db_test_con(), with_test_db(test_con))
