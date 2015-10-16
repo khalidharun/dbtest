35902f0d96f3d1b71b77893da1994d12763c1899
Peter Hurford <peter@peterhurford.com>
2015-10-16 14:53:34 -0500

    Steal db functions from robertzk/cachemeifyoucan

---
+++ R/dbtest.R
@@ -0,0 +1,19 @@
+database.yml <- system.file(package = "dbtest", "database.yml")
+
+#' Create a connection to a test database for testing database transactions.
+#' @return a database connection suitable for testing.
+#' @export
+db_test_con <- function() {
+  conn <- try(silent = TRUE, db_connection(database.yml))
+  if (is(conn, 'try-error')) {
+    stop("Cannot run tests until you create a database named ", sQuote("travis"),
+         "for user ", sQuote("postgres"), ". (You should be able to open ",
+         "the PostgreSQL console using ", dQuote("psql postgres"),
+         " from the command line. ",
+         "From within there, run ", dQuote(paste0("CREATE DATABASE travis; ",
+         "GRANT ALL PRIVILEGES ON DATABASE travis TO postgres;")),
+         " You might also need to run ", dQuote("ALTER ROLE postgres WITH LOGIN;"),
+         ")", call. = FALSE)
+  }
+  conn
+}
