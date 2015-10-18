dab76def466cb4cfba1c591a8e40f20c6073b393
Peter Hurford <peter@peterhurford.com>
2015-10-18 04:41:53 -0500

    Sketch some helper ideas II

--- R/assert.R
+++ R/assert.R
@@ -1,3 +1,4 @@
-# assert_db_has(table("flights"))
-# assert_db_has(count("flights") > 0)
-# assert_table_has("flights", count("id") > 0)
+# expect_db_has(table("flights"))
+# expect_db_has(count("flights") > 0)
+# expect_table_has("flights", count("id") > 0)
+# expect_equal_with_sql(""
--- assert.R
+++
@@ -1,3 +0,0 @@
-# assert_db_has(table("flights"))
-# assert_db_has(count("flights") > 0)
-# assert_table_has("flights", count("id") > 0)
