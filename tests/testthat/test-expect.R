e0de19a779e2f412885750457606a18c175c4817
Peter Hurford <peter@peterhurford.com>
2015-10-18 07:24:48 -0500

    move mocked_testthat to a helper

--- tests/testthat/test-expect.R
+++ tests/testthat/test-expect.R
@@ -3,29 +3,6 @@ context("expect")
 old_verbose <- options()$dbtest.verbose
 options(dbtest.verbose = FALSE)
 
-#TODO: Move this to a helper file
-with_mocked_testthat <- function(expr) {
-  testthat::with_mock(`testthat::get_reporter` = function(...) {
-    list(add_result = function(...) { NULL }) }, expr) }
-
-test_passed <- function(expr) {
-  with_mocked_testthat(expr)$passed
-}
-
-get_failure_message <- function(expr) {
-  strsplit(with_mocked_testthat(expr)$failure_msg, "\n")[[1]]
-}
-
-expect_failed_test <- function(test) {
-  expect_false(test_passed(test))
-}
-
-describe("with_mocked_testthat", {
-  test_that("it can extract an error without erroring", {
-    expect_failed_test(expect_true(FALSE))
-  })
-})
-
 
 with_test_db({
   describe("expect_table", {
